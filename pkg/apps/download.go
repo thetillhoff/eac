4e0aae44f1a4849826f420a7609b9ee07e67860e
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-01-11 21:33:51 +0100

    added download for all app manifests

--- pkg/apps/download.go
+++ pkg/apps/download.go
@@ -9,9 +9,14 @@ import (
 
 	"github.com/thetillhoff/eac/internal/config"
 	"github.com/thetillhoff/eac/pkg/apps/internal/app"
+	"github.com/thetillhoff/eac/pkg/logs"
 )
 
 func downloadFile(url string, destination string) {
+
+	logs.Info("url:", url)
+	logs.Info("destination:", destination)
+
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln(err)
@@ -31,5 +36,9 @@ func downloadFile(url string, destination string) {
 }
 
 func downloadApp(conf config.Config, app app.App, platform string) {
-	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/getLatestVersion.sh", path.Join(AppsDirPath, app.Name, platform, "getLatestVersion.sh"))
+	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/getLatestVersion.sh", path.Join(conf.AppsDirPath, app.Name, platform, "getLatestVersion.sh"))
+	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/getLocalVersion.sh", path.Join(conf.AppsDirPath, app.Name, platform, "getLocalVersion.sh"))
+	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/install.sh", path.Join(conf.AppsDirPath, app.Name, platform, "install.sh"))
+	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/configure.sh", path.Join(conf.AppsDirPath, app.Name, platform, "configure.sh"))
+	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/uninstall.sh", path.Join(conf.AppsDirPath, app.Name, platform, "uninstall.sh"))
 }
