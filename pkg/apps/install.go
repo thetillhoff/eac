fa9cd46c54ec21a40175ece616e19f66946dc9f6
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-02-13 16:32:29 +0100

    more logs for install command

--- pkg/apps/install.go
+++ pkg/apps/install.go
@@ -34,19 +34,21 @@ func Install(appNames []string, conf config.Config) {
 			appItem.WantedVersion = appItem.LatestVersion(conf.AppsDirPath, runtime.GOOS) // Set WantedVersion to the latest available one
 			appVersions.SetVersion(appItem.Name, appItem.WantedVersion)
 			appVersions.Save(conf.VersionsFilePath)
-			logs.Info("Due to flag 'latest', the wantedVersion of app '" + appItem.Name + "' was set to 'v" + appItem.WantedVersion + "'.")
+			logs.Info("Explicit latest tag used for app '" + appItem.Name + "'.")
+			logs.Info("Persisted version for app '" + appItem.Name + "' to latest 'v" + appItem.WantedVersion + "'.")
 		} else if appItem.WantedVersion == "" { // If no version was specified
 			appItem.WantedVersion = appItem.LatestVersion(conf.AppsDirPath, runtime.GOOS) // Set WantedVersion to the latest available one
 			appVersions.SetVersion(appItem.Name, appItem.WantedVersion)
 			appVersions.Save(conf.VersionsFilePath)
-			logs.Info("Version for app '" + appItem.Name + "' was automatically set to latest 'v" + appItem.WantedVersion + "'.")
-		}
-		// implicit else:
-		//   pass version already existant in app.WantedVersion (f.e. from versionsFile)
+			logs.Info("Implicit latest tag used for app '" + appItem.Name + "'.")
+			logs.Info("Persisted version for app '" + appItem.Name + "' to latest 'v" + appItem.WantedVersion + "'.")
+		} // Else version already exists in app.WantedVersion (f.e. from versionsFile)
 
+		logs.Info(appItem.Name+" Installed: "+appItem.InstalledVersion(conf.AppsDirPath)+", Wanted: "+appItem.WantedVersion, nil)
 		if appItem.InstalledVersion(conf.AppsDirPath) == appItem.WantedVersion { // If app is already installed in desired version
 			logs.Success("App '" + appItem.Name + "' is already installed in wanted version '" + appItem.WantedVersion + "'.")
 		} else { // app is either not installed, or installed in wrong version
+
 			appItem, out, err := appItem.Install(conf.AppsDirPath, runtime.GOOS, "") // Install app
 
 			if err == nil {
