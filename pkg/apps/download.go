6435dfeb7f11427d8ca9a8a1fa8103aabf55eaed
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-02-13 16:28:41 +0100

    fix filepaths for scripts

--- pkg/apps/download.go
+++ pkg/apps/download.go
@@ -38,7 +38,7 @@ func downloadFile(url string, destination string) {
 func downloadApp(conf config.Config, app app.App, platform string) {
 	logs.Info("Downloading files for app '" + app.Name + "'")
 	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/getLatestVersion.sh", path.Join(conf.AppsDirPath, app.Name, platform, "getLatestVersion.sh"))
-	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/getLocalVersion.sh", path.Join(conf.AppsDirPath, app.Name, platform, "getLocalVersion.sh"))
+	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/getInstalledVersion.sh", path.Join(conf.AppsDirPath, app.Name, platform, "getInstalledVersion.sh"))
 	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/install.sh", path.Join(conf.AppsDirPath, app.Name, platform, "install.sh"))
 	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/configure.sh", path.Join(conf.AppsDirPath, app.Name, platform, "configure.sh"))
 	downloadFile("https://raw.githubusercontent.com/thetillhoff/eac/main/apps/"+app.Name+"/"+platform+"/uninstall.sh", path.Join(conf.AppsDirPath, app.Name, platform, "uninstall.sh"))
