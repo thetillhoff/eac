4d1a3e6e2c474806180123854fbb29b391af1671
Till Hoffmann <till.hoffmann@thetillhoff.de>
2022-02-13 16:32:01 +0100

    parse appnames with ==<version>

--- pkg/apps/parseApps.go
+++ pkg/apps/parseApps.go
@@ -1,10 +1,12 @@
 package apps
 
 import (
+	"os"
 	"strings"
 
 	"github.com/thetillhoff/eac/pkg/apps/internal/app"
 	"github.com/thetillhoff/eac/pkg/apps/internal/appVersions"
+	"github.com/thetillhoff/eac/pkg/logs"
 )
 
 // Parses the provided slice of strings and returns a list of apps.
@@ -16,9 +18,12 @@ func parseApps(appNames []string, versionsFilePath string) []app.App {
 
 	for _, appName := range appNames {
 		wantedVersion := ""
-		if strings.Contains(appName, "=") {
-			splitted := strings.Split(appName, "=")
+		if strings.Contains(appName, "==") {
+			splitted := strings.Split(appName, "==")
 			appName, wantedVersion = splitted[0], splitted[1]
+		} else if strings.Contains(appName, "=") {
+			logs.Error("Invalid appname '" + appName + "'.\nUse '==' if you want to specify a version.")
+			os.Exit(1)
 		}
 		if wantedVersion == "" { // if wantedVersion is not set via `<app>=<version>`
 			wantedVersion = appVersions.GetVersion(appName) // retrieve wantedVersion from file
